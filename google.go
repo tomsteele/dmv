9ebb2f24b1cfe94428781dc0555427a3b3545903
James Vastbinder <james.vastbinder@gmail.com>
2014-03-25 16:14:58 -0700

    ran fmt to insert tabs

--- google.go
+++ google.go
@@ -1,34 +1,34 @@
 package dmv
 
 import (
-  "code.google.com/p/goauth2/oauth"
-  "encoding/json"
-  "github.com/codegangsta/martini"
-  "io/ioutil"
-  "net/http"
-  "net/url"
-  "strings"
+	"code.google.com/p/goauth2/oauth"
+	"encoding/json"
+	"github.com/codegangsta/martini"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 var (
-  // google oauth2 endpoints
-  googleProfileURL = "https://www.googleapis.com/oauth2/v1/userinfo"
+	// google oauth2 endpoints
+	googleProfileURL = "https://www.googleapis.com/oauth2/v1/userinfo"
 )
 
 // Google holds the access and refresh tokens along with the user profile
 type Google struct {
-  Errors       []error
-  AccessToken  string
-  RefreshToken string
-  Profile      GoogleProfile
+	Errors       []error
+	AccessToken  string
+	RefreshToken string
+	Profile      GoogleProfile
 }
 
 type GoogleProfile struct {
-  ID          string `json:"id"`
-  DisplayName string `json:"name"`
-  FamilyName  string `json:"family_name"`
-  GivenName   string `json:"given_name"`
-  Email       string `json:"email"`
+	ID          string `json:"id"`
+	DisplayName string `json:"name"`
+	FamilyName  string `json:"family_name"`
+	GivenName   string `json:"given_name"`
+	Email       string `json:"email"`
 }
 
 // AuthGoogle authenticates users using Google and OAuth2.0. After handling
@@ -83,58 +83,58 @@ type GoogleProfile struct {
 //     }
 
 func AuthGoogle(opts *OAuth2Options) martini.Handler {
-  opts.AuthURL = "https://accounts.google.com/o/oauth2/auth"
-  opts.TokenURL = "https://accounts.google.com/o/oauth2/token"
-  config := &oauth.Config{
-    ClientId:     opts.ClientID,
-    ClientSecret: opts.ClientSecret,
-    RedirectURL:  opts.RedirectURL,
-    Scope:        strings.Join(opts.Scopes, " "),
-    AuthURL:      opts.AuthURL,
-    TokenURL:     opts.TokenURL,
-  }
+	opts.AuthURL = "https://accounts.google.com/o/oauth2/auth"
+	opts.TokenURL = "https://accounts.google.com/o/oauth2/token"
+	config := &oauth.Config{
+		ClientId:     opts.ClientID,
+		ClientSecret: opts.ClientSecret,
+		RedirectURL:  opts.RedirectURL,
+		Scope:        strings.Join(opts.Scopes, " "),
+		AuthURL:      opts.AuthURL,
+		TokenURL:     opts.TokenURL,
+	}
 
-  transport := &oauth.Transport{
-    Config:    config,
-    Transport: http.DefaultTransport,
-  }
+	transport := &oauth.Transport{
+		Config:    config,
+		Transport: http.DefaultTransport,
+	}
 
-  cbPath := ""
-  if u, err := url.Parse(opts.RedirectURL); err == nil {
-    cbPath = u.Path
-  }
-  return func(r *http.Request, w http.ResponseWriter, c martini.Context) {
-    if r.URL.Path != cbPath {
-      http.Redirect(w, r, transport.Config.AuthCodeURL(""), http.StatusFound)
-      return
-    }
-    goog := &Google{}
-    defer c.Map(goog)
-    code := r.FormValue("code")
-    tk, err := transport.Exchange(code)
-    if err != nil {
-      goog.Errors = append(goog.Errors, err)
-      return
-    }
-    goog.AccessToken = tk.AccessToken
-    goog.RefreshToken = tk.RefreshToken
-    resp, err := transport.Client().Get(googleProfileURL)
-    if err != nil {
-      goog.Errors = append(goog.Errors, err)
-      return
-    }
-    defer resp.Body.Close()
-    profile := &GoogleProfile{}
-    data, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-      goog.Errors = append(goog.Errors, err)
-      return
-    }
-    if err := json.Unmarshal(data, profile); err != nil {
-      goog.Errors = append(goog.Errors, err)
-      return
-    }
-    goog.Profile = *profile
-    return
-  }
+	cbPath := ""
+	if u, err := url.Parse(opts.RedirectURL); err == nil {
+		cbPath = u.Path
+	}
+	return func(r *http.Request, w http.ResponseWriter, c martini.Context) {
+		if r.URL.Path != cbPath {
+			http.Redirect(w, r, transport.Config.AuthCodeURL(""), http.StatusFound)
+			return
+		}
+		goog := &Google{}
+		defer c.Map(goog)
+		code := r.FormValue("code")
+		tk, err := transport.Exchange(code)
+		if err != nil {
+			goog.Errors = append(goog.Errors, err)
+			return
+		}
+		goog.AccessToken = tk.AccessToken
+		goog.RefreshToken = tk.RefreshToken
+		resp, err := transport.Client().Get(googleProfileURL)
+		if err != nil {
+			goog.Errors = append(goog.Errors, err)
+			return
+		}
+		defer resp.Body.Close()
+		profile := &GoogleProfile{}
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			goog.Errors = append(goog.Errors, err)
+			return
+		}
+		if err := json.Unmarshal(data, profile); err != nil {
+			goog.Errors = append(goog.Errors, err)
+			return
+		}
+		goog.Profile = *profile
+		return
+	}
 }
